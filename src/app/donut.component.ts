98edb71f7ddf75d645d76189a61226ec35ae6637
Thinh Mazeika <thinh.mazeika@gmail.com>
2021-01-05 17:11:13 -0500

    property binding

--- src/app/donut.component.ts
+++ src/app/donut.component.ts
@@ -4,13 +4,12 @@ import { Donut } from './models/donut.interface';
 @Component({
   selector: 'app-donut',
   template: `
-    <div class="name">
-      <!-- Hide the icing label when a donut has a fileName -->
-      {{ donut.name }} <span *ngIf="donut.icing">(icing)</span>
+    <div class="name" *ngIf="donut.fileName">
+      <img [src]="donut?.fileName" [alt]="donut.name" />
+
+      {{ donut.name }} <span *ngIf="!donut.fileName">(icing)</span>
     </div>
-    <!-- Add image displaying the donut.fileName when specified. -->
-    <!-- Then, add the alt attribute to the image using the donut.name value. -->
-  `
+  `,
 })
 export class DonutComponent {
   @Input() donut: Donut;
